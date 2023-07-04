30025eb9665e24dcecf0dfa454872cb08394c0f8
root <root@en214.eth>
2023-07-03 18:07:04 -0600

    test

--- src/python/phenix_apps/apps/sceptre/__init__.py
+++ src/python/phenix_apps/apps/sceptre/__init__.py
@@ -1,5 +1,5 @@
 import os
-import phenix_apps.apps.sceptre.configs
+import phenix_apps.apps.sceptre.configs.configs
 import phenix_apps.apps.sceptre.protocols
 
 DISABLE_REG_MAP_GEN = os.getenv('PHENIX_APPS_SCEPTRE_DISABLE_REG_MAP', None) != None
